791e02d3f12ae04b33f1ae691a82c3ffc4548e5d
Arielle Morris <7.morrisarielle@gmail.com>
2022-02-22 10:19:25 -0600

    contact

--- Contact/contact.php
+++ Contact/contact.php
@@ -1,23 +1,51 @@
 <?php
 
 
-    $firstName = $_POST['firstName'];
-    $lastName = $_POST['lastName'];
-    $message = $_POST['message'];
-    $budget = $_POST['budget'];
-    $email = $_POST['email'];
-    $number = $_POST['number'];
+    $firstName = filter_input($INPUT_POST, 'firstName');
+    $lastName = filter_input($INPUT_POST, 'lastName');
+    $message = filter_input($INPUT_POST, 'message');
+    $email = filter_input($INPUT_POST, 'email');
+    $number = filter_input($INPUT_POST, 'number');
+
 
     if (!empty($firstName) || !empty($lastName) || !empty($message) 
     || !empty($email) || !empty($number)) {
-    $host = "localhost";
-    $dbUsername = "	u307258664_ariellemorris";
-    $dbPassword = "Naruto1#";
-    $dbName = "u307258664_database_one";
-    } else{
-        echo "All fields are required";
-        die();
+        $host = "localhost";
+        $dbUsername = "	u307258664_ariellemorris";
+        $dbPassword = "Naruto1#";
+        $dbName = "u307258664_mydatabase";
+
+        //create connection
+        $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
+        if(mysqli_connect_error()){
+            die('Connect Error ('. mysqli_connect_error() . ')'
+            . mysqli_connection_error());
+        }
+        else{
+            $sql = "INSERT INTO contact (firstName, lastName, message, number, email) values ('$firstName', '$lastName', '$message', '$email', '$number')";
+            if($conn->query($sql)){
+                echo "New connection successful";
+        }
     }
+
+
+
+    }
+
+    
+   
+      
+
+    
+        
+ 
+
+     
+    
+  
+
+   
+
     
 
 
