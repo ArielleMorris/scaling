235491412ff41c27eb498c04555ef69b9b83f96f
Arielle Morris <7.morrisarielle@gmail.com>
2022-02-23 21:46:06 -0600

    contact2

--- Contact/contact.php
+++ Contact/contact.php
@@ -1,8 +1,8 @@
 <?php
 
-
-    $firstName = filter_input($INPUT_POST, 'firstName');
-    $lastName = filter_input($INPUT_POST, 'lastName');
+/*
+    $firstName = ($_POST, 'firstName');
+    $lastName = ($INPUT_POST, 'lastName');
     $message = filter_input($INPUT_POST, 'message');
     $email = filter_input($INPUT_POST, 'email');
     $number = filter_input($INPUT_POST, 'number');
@@ -12,11 +12,10 @@
     || !empty($email) || !empty($number)) {
         $host = "localhost";
         $dbUsername = "	u307258664_ariellemorris";
-        $dbPassword = "Naruto1#";
         $dbName = "u307258664_mydatabase";
 
         //create connection
-        $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
+        $conn = new mysqli($host, $dbUsername, $dbName);
         if(mysqli_connect_error()){
             die('Connect Error ('. mysqli_connect_error() . ')'
             . mysqli_connection_error());
@@ -31,6 +30,21 @@
 
 
     }
+
+    */
+
+
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
+    $message = $_POST['message'];
+    $formcontent = "From: $name \n Message: $message";
+    $recipient = "7.morrisarielle@gmail.com";
+    $subject = "Contact Form";
+    $mailheader = "From: $email \r\n";
+    mail($recipient, $subject, $formcontent, $mailheader) or die("Error");
+    echo "Thank you";
+
+
 
     
    
